8f53f1377e72337b118ee62b1e3c8a213fe13ea9
skudryavtsev <s2.kudryavtsev@itransition.com>
2019-01-10 23:07:06 +0300

    build

---
+++ precache-manifest.59bb846dfdc12ebd00f387eeab5894f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7847fc47f142f0ba2fbd",
+    "url": "/calculator-react-redux-typescript/static/js/runtime~main.7847fc47.js"
+  },
+  {
+    "revision": "4e8357dd935d8b2bf19b",
+    "url": "/calculator-react-redux-typescript/static/js/main.4e8357dd.chunk.js"
+  },
+  {
+    "revision": "01aceb64aa2553391566",
+    "url": "/calculator-react-redux-typescript/static/js/1.01aceb64.chunk.js"
+  },
+  {
+    "revision": "4e8357dd935d8b2bf19b",
+    "url": "/calculator-react-redux-typescript/static/css/main.b2acf9ef.chunk.css"
+  },
+  {
+    "revision": "489796e1f755151cffab1880b63dff23",
+    "url": "/calculator-react-redux-typescript/index.html"
+  }
+];
